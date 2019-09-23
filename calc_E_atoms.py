27ef3da321e6a865d279ff523ee3014e6b9f5ae2
Etienne <etienne.cuierrier@umontreal.ca>
2019-09-23 11:23:32 -0400

    now it extracts pbe exc from the calculation

--- calc_E_atoms.py
+++ calc_E_atoms.py
@@ -6,26 +6,28 @@ import sys
 """
 Description:
     This code can be used to compute the total energies of various atoms from H to Ar
-    with PBE/6-311+g(2d,p) or Kohn-Sham exact exchange energy density post-PBE.
+    with functional/6-311+g(2d,p) or Kohn-Sham exact exchange energy density post-PBE.
     To use it, it is important to download the basis set from https://www.basissetexchange.org
     and name it 6-311+g2dp.nw.
+    For the implementation of kohn-Sham exact exchange energy density, see the appendix of
+    https://doi.org/10.1063/1.5083840 .
 
-Usage: python3 calc_E_atom.py method  (methos id PBE or EXKS)
+Usage: python3 calc_E_atom.py functional  (functional is a function from pySCF or EXKS)
     It will create a file named E_atoms.txt with all the energies.
 
 TODO:
-    Make it more flexible to chose different functionnals and basis sets
-    A better way to compute total exchange energy density by not having to calculate Exc of PBE
-        since useless calculation is done
+    Make it more flexible to chose different basis sets
     A prettier format for E_atom.txt
 """
 
-def calc_energy_pbe(atom,spin):
+def calc_energy_dft(atom,spin,functional):
     """
-    Calculate the total energies of an atom with pbe/6-311+g(2d,p).
+    Calculate the total energies of an atom with functional/6-311+g(2d,p).
+    The energies are computed from PBE converged density (post-PBE method)
     Input:
         atom(string): the element symbol
         spin(int): The total spin
+        functional(string): functional name in pyscf format
     returns:
         energy(float): the total energy
     """
@@ -37,14 +39,16 @@ def calc_energy_pbe(atom,spin):
     mol.build()
     mf = scf.KS(mol)
     mf.small_rho_cutoff = 1e-12
-    mf.xc='pbe'
+    mf.xc=functional
     mf.kernel()
+    pbe_exc=mf.get_veff().exc
     return mf.e_tot
 
 
 def compute_ex_exact(mol,ao_value,dm,coord):
     """
     Function to compute the exact kohn sham exchange energy density.
+    See the appendix of https://doi.org/10.1063/1.5083840 for details.
     Input:
         mol(mol): molecule object from pyscf
         ao_values(array): ao values for a grid point
@@ -60,14 +64,13 @@ def compute_ex_exact(mol,ao_value,dm,coord):
 
 def calc_energy_exks(atom,spin):
     """
-    Calculate the total energy when using kohn sham exact exchange
+    Calculate the total energy when using kohn sham exact exchange post PBE
 
     Input:
         atom(string): element symbol
         spin(int): Spin of the atom
     TODO
-        Finding a way in PYSCF to extract all the contribution to the total energy
-        so Exc PBE won't have to be recalculated
+
     """
     mol = gto.Mole()
     mol.atom=atom
@@ -79,15 +82,12 @@ def calc_energy_exks(atom,spin):
     mf.small_rho_cutoff = 1e-12
     mf.xc='pbe'
     mf.kernel()
+    pbe_exc=mf.get_veff().exc
     grids = mf.grids
     #pbe
     ao_value = numint.eval_ao(mol, grids.coords, deriv=1)
     dm = mf.make_rdm1()
     if spin==0:
-        rho = numint.eval_rho(mol, ao_value, dm, xctype='GGA')
-        ex, vx = dft.libxc.eval_xc('pbe,', rho)[:2]
-        ec, vc = dft.libxc.eval_xc(',pbe', rho)[:2]
-        Exc = np.einsum('i,i,i->', ex+ec, rho[0],grids.weights)
         #EX exact
         nGrid = np.shape(grids.coords)[0]
         exExact=np.zeros(nGrid)
@@ -96,18 +96,7 @@ def calc_energy_exks(atom,spin):
                                     dm,grids.coords[iG])/2.
         Extot= np.einsum('i,i->', exExact,grids.weights)
     else:# for spin polarized molecule
-        #pbe
-        rhoA = numint.eval_rho(mol, ao_value, dm[0], xctype='GGA')
-        zeros=np.zeros(np.shape(rhoA))
-        rhoB = numint.eval_rho(mol, ao_value, dm[1], xctype='GGA')
-        exA, vx = dft.libxc.eval_xc('pbe,', [rhoA,zeros],spin=spin)[:2]
-        exB, vx = dft.libxc.eval_xc('pbe,', [zeros,rhoB],spin=spin)[:2]
-        ec, vc = dft.libxc.eval_xc(',pbe', [rhoA,rhoB],spin=spin)[:2]
-        ExA = np.einsum('i,i,i->', exA, rhoA[0],grids.weights)
-        ExB = np.einsum('i,i,i->', exB, rhoB[0],grids.weights)
-        Ec = np.einsum('i,i,i->', ec, rhoA[0]+rhoB[0],grids.weights)
-        Exc = ExA+ExB+Ec
-        #EX
+        #EX exact
         nGrid = np.shape(grids.coords)[0]
         exExactA=np.zeros(nGrid)
         exExactB=np.zeros(nGrid)
@@ -119,7 +108,7 @@ def calc_energy_exks(atom,spin):
         Extot= np.einsum('i,i->', exExactA+exExactB, 
                                          grids.weights)
     #ex exact
-    ETotalExKS = mf.e_tot-Exc+Extot
+    ETotalExKS = mf.e_tot-pbe_exc+Extot
     return ETotalExKS
 
 lib.num_threads(1)# pySCF will only use 1 thread
@@ -128,15 +117,13 @@ atoms={"H":1,"He":0,"Li":1,"Be":0,"B":1,"C":2,"N":3,
         "O":2,"F":1,"Ne":0,"Na":1,"Ne":0,"Na":1,
         "Mg":0,"Al":1,"Si":2,"P":3,"S":2,"Cl":1,"Ar":0}
         
-implemented_method=["PBE","EXKS"]
-method = sys.argv[1]
-assert method in implemented_method,"Method must be PBE or EXKS"
-if method=="PBE": energy=calc_energy_pbe
-if method=="EXKS":energy = calc_energy_exks
-
+functional = sys.argv[1]
 #file stuff
 f=open("E_atom.txt","w")
-f.write("Atom "+method+"\n")
+f.write("Atom "+functional+"\n")
 for atom in atoms:
-    f.write(atom +" %.8f\n"%energy(atom,atoms[atom]))
+    if functional=="EXKS":
+        f.write(atom +" %.8f\n"%calc_energy_exks(atom,atoms[atom]))
+    else:
+        f.write(atom +" %.8f\n"%calc_energy_dft(atom,atoms[atom],functional))
 f.close()
