ec4de938df6219ac7ab3fb5937380f438015b3f9
polivierroy <47262801+polivierroy@users.noreply.github.com>
2019-10-11 14:16:30 -0400

    Added coupute_coulomb

--- energy_densities.py
+++ energy_densities.py
@@ -16,4 +16,10 @@ def compute_ex_exact(mol,ao_value,DMA,coords):
        A = mol.intor('int1e_rinv')
     F = np.dot(DMA,ao_value)
     return -.5*np.einsum('i,j,ij',F,F,A)
+   
+ def compute_coulomb(mol,ao_value,DMA,coords):
+    # Coulomb potential from a spin-density. This returns energy density per particle, NOT THE ENERGY DENSITY. 
+    with mol.with_rinv_origin((coords[0],coords[1],coords[2])):
+       A = mol.intor('int1e_rinv')
+    return -.5*np.einsum('ij,ij',DMA,A)
     
