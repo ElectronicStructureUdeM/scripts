73c7ab1260a2eb7d6d4ce9d382f0b71ca80db823
Etienne <etienne.cuierrier@umontreal.ca>
2019-12-04 16:42:11 -0500

    cfxn mostly works. todo cfx and the  check if the parameters of br are negative

--- CFXpo.py
+++ CFXpo.py
@@ -65,27 +65,6 @@ class CFXN(ModelXC):
         return  brholedtot(self.y_values,self.zeta[gridID],br_a_up,br_b_up,br_c_up,br_d_up,
                                 br_a_down,br_b_down,br_c_down,br_d_down)
 
-    def calc_eps_xc_cfxn(self,gridID):
-        # for jx lsd
-        self.JX_LSD = self.calc_jx_approx(gridID,self.eps_x_LSD_up[gridID],self.eps_x_LSD_down[gridID])
-        # for fc lsd
-        self.A = self.calc_A(self.rs[gridID],self.zeta[gridID])
-        self.B = self.calc_B(self.rs[gridID],self.zeta[gridID])
-        self.E=self.calc_E(self.rho_tot[gridID],self.kf[gridID],self.eps_xc_LSD[gridID])
-
-        #for jx pbe
-        self.JX_PBE = self.calc_jx_approx(gridID,self.eps_x_PBE_up[gridID],self.eps_x_PBE_down[gridID])
-
-        #for C,D pbe
-        self.calc_CD(self.rho_tot[gridID],self.kf[gridID],self.eps_xc_PBE[gridID])
-        return self.eps_xc_PBE_calc
-   
-    def calc_Exc_cfxn(self):
-        sum=0.
-        for gridID in range(self.n_grid):
-            sum+=self.weights[gridID]*self.rho_tot[gridID]*self.calc_eps_xc_cfxn(gridID)
-        print(sum)
-
     def calc_A(self,rs,zeta):
         """
         Calculate A which is used to reproduce the on-top value of rho_xc of LSD
@@ -151,11 +130,51 @@ class CFXN(ModelXC):
                     [m3,m5]])
         b=np.array([-(3.*np.pi/4.+self.A*m2+self.B*m3),
                     eps_xc*kf**2/(2.*np.pi*rho)-self.A*m1-self.B*m2])
-        self.C,self.D = np.linalg.solve(a, b)
-        self.eps_xc_PBE_calc=2.*np.pi*rho/kf**2*(self.A*m1+self.B*m2+self.C*m3+self.D*m5)
+        C,D = np.linalg.solve(a, b)
+        self.eps_xc_PBE_calc=2.*np.pi*rho/kf**2*(self.A*m1+self.B*m2+C*m3+D*m5)
+        return C,D
+
+    def calc_eps_xc_cfxn(self,gridID):
+        # for jx lsd
+        self.JX_LSD = self.calc_jx_approx(gridID,self.eps_x_LSD_up[gridID],self.eps_x_LSD_down[gridID])
+        # for fc lsd
+        self.A = self.calc_A(self.rs[gridID],self.zeta[gridID])
+        self.B = self.calc_B(self.rs[gridID],self.zeta[gridID])
+        self.E=self.calc_E(self.rho_tot[gridID],self.kf[gridID],self.eps_xc_LSD[gridID])
+
+        #for jx pbe
+        self.JX_PBE = self.calc_jx_approx(gridID,self.eps_x_PBE_up[gridID],self.eps_x_PBE_down[gridID])
+
+        #for C,D pbe
+        self.C,self.D=self.calc_CD(self.rho_tot[gridID],
+                                                self.kf[gridID],self.eps_xc_PBE[gridID])
+        
+        #for jx exact
+        self.JX_Exact = self.calc_jx_approx(gridID,self.eps_x_exact_up[gridID],
+                                                  self.eps_x_exact_down[gridID])
+        #to calculate C and energy 
+        m1 = self.moment_JX_E(self.E,1,self.JX_Exact)
+        m2 = self.moment_JX_E(self.E,2,self.JX_Exact)
+        m3 = self.moment_JX_E(self.E,3,self.JX_Exact)
+        m4 = self.moment_JX_E(self.E,4,self.JX_Exact)
+        m5 = self.moment_JX_E(self.E,5,self.JX_Exact)
+        m6 = self.moment_JX_E(self.E,6,self.JX_Exact)
+        self.C = -(3.*np.pi/4.+m2*self.A+m3*self.B+m6*self.D)/m4
+        self.eps_xc_calc = 2.*np.pi*self.rho_tot[gridID]/self.kf[gridID]**2*(self.A*m1+
+                                        self.B*m2+self.C*m3+self.D*m5)
+
+        return self.eps_xc_calc-(self.eps_x_exact_up[gridID]*self.rho_up[gridID]+self.eps_x_exact_down[gridID]*self.rho_down[gridID])/self.rho_tot[gridID]
+   
+    def calc_Exc_cfxn(self):
+        sum=0.
+        for gridID in range(self.n_grid):
+            sum+=self.weights[gridID]*self.rho_tot[gridID]*self.calc_eps_xc_cfxn(gridID)
+        print(sum)
 
 
-test = CFXN('He',[],0,basis = 'cc-pvtz',ASE=False)
+
+
+test = CFXN('Ar',[],0,basis = 'cc-pvtz',ASE=False)
 test.calc_Exc_cfxn()
 
 
