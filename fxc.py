ff0d75cc3b99bf4e66f8d84204b82c2f3bf61c8e
Etienne <etienne.cuierrier@umontreal.ca>
2020-02-17 14:59:57 -0500

    seems to work, but Li2 results are strange

--- fxc.py
+++ fxc.py
@@ -32,18 +32,19 @@ class Fxc(ModelXC):
         self.eps_xc_post_approx = self.exc_post_approx/self.rho_tot
 
 
-    @vectorize([float64(float64,float64,float64,float64,float64,float64,float64)])
-    def calc_fx4(alpha,beta,xi,gamma,u2,u4,u6):
+    @vectorize([float64(float64,float64,float64,float64,float64,float64,float64,float64,float64)])
+    def calc_fx4(alpha,beta,xi,gamma,u1,u2,u4,u6,umax):
         """
-        Calculate fx4:(-1+alpha*u**2+beta*u**4+xi*u**4)*exp(-gamma*u**2)
+        Calculate fx4
         input:
             alpha,beta,xi,gamma:
             parameters
-        u2,u4,u6:
-            power of u
-
+        u1,u2,u4,u6:
+            power of u, precalculated to save time
+        umax:
+            u corresponding to the max value of rho(r,u)
         """
-        return (-1.+alpha*u2+beta*u4+xi*u6)*np.exp(-gamma*u2)
+        return np.exp(-gamma*u2)*(-1.+alpha*u2)+(beta*u4+xi*u6)*np.exp(-gamma*(u1-umax)**2)
 
     def find_gamma(self,gamma,norm,rhoRU):
         """
@@ -61,8 +62,8 @@ class Fxc(ModelXC):
                int_0^maxu 4*pi*u**2*rho(r,u)*fx4 du - norm
 
         """
-        fx4 = self.calc_fx4(0.,0.,0.,gamma,self.ux_pow[2],0.,0.)
-        
+        fx4 = self.calc_fx4(0.,0.,0.,gamma,0.,self.ux_pow[2],0.,0.,0.)
+        #print(gamma, 4.*np.pi*np.einsum("i,i,i->",self.uwei,self.ux_pow[2],fx4*rhoRU),norm)
         return 4.*np.pi*np.einsum("i,i,i->",self.uwei,self.ux_pow[2],fx4*rhoRU)-norm
 
 
@@ -85,26 +86,28 @@ class Fxc(ModelXC):
                 the exchange factor for each u
         """
         #gamma
-        gamma= scipy.optimize.brentq(self.find_gamma,-1e-3,1000,args=(norm,rhoRU))
+        gamma= scipy.optimize.brentq(self.find_gamma,-1e-1,1000,args=(norm,rhoRU))
         #alpha
         alpha = (-Q+(1./6.)*lap)/(2.*rhoR)-gamma
-        #beta and xi
+        #beta
+        umax_ind = np.argmax(rhoRU)
+        umax =0.# self.ux[umax_ind]
         m1 = np.einsum("i,i,i,i->",self.uwei,self.ux_pow[1],rhoRU,np.exp(-gamma*self.ux_pow[2]))
         m2 = np.einsum("i,i,i,i->",self.uwei,self.ux_pow[2],rhoRU,np.exp(-gamma*self.ux_pow[2]))
         m3 = np.einsum("i,i,i,i->",self.uwei,self.ux_pow[3],rhoRU,np.exp(-gamma*self.ux_pow[2]))
         m4 = np.einsum("i,i,i,i->",self.uwei,self.ux_pow[4],rhoRU,np.exp(-gamma*self.ux_pow[2]))
-        m5 = np.einsum("i,i,i,i->",self.uwei,self.ux_pow[5],rhoRU,np.exp(-gamma*self.ux_pow[2]))
-        m6 = np.einsum("i,i,i,i->",self.uwei,self.ux_pow[6],rhoRU,np.exp(-gamma*self.ux_pow[2]))
-        m7 = np.einsum("i,i,i,i->",self.uwei,self.ux_pow[7],rhoRU,np.exp(-gamma*self.ux_pow[2]))
-        m8 = np.einsum("i,i,i,i->",self.uwei,self.ux_pow[8],rhoRU,np.exp(-gamma*self.ux_pow[2]))
-        #
-        a_data = [[m6,m8],
-                  [m5,m7]]
-        b_data = [norm/(4.*np.pi)+m2-alpha*m4,
-                    epsilonX/(2.*np.pi)+m1-alpha*m3]
-        beta,xi = np.linalg.solve(a_data, b_data)
-        fx4 = self.calc_fx4(alpha,beta,xi,gamma,self.ux_pow[2],
-                            self.ux_pow[4],self.ux_pow[6])
+        m5_umax = np.einsum("i,i,i,i->",self.uwei,self.ux_pow[5],rhoRU,np.exp(-gamma*(self.ux_pow[1]-umax)**2))
+        m6_umax = np.einsum("i,i,i,i->",self.uwei,self.ux_pow[6],rhoRU,np.exp(-gamma*(self.ux_pow[1]-umax)**2))
+        m7_umax = np.einsum("i,i,i,i->",self.uwei,self.ux_pow[7],rhoRU,np.exp(-gamma*(self.ux_pow[1]-umax)**2))
+        m8_umax = np.einsum("i,i,i,i->",self.uwei,self.ux_pow[8],rhoRU,np.exp(-gamma*(self.ux_pow[1]-umax)**2))
+        a_data=[[m6_umax,m8_umax],
+                [m5_umax,m7_umax]]
+        b_data=[norm/(4.*np.pi)+m2-alpha*m4,
+                epsilonX/(2.*np.pi)+m1-alpha*m3]
+        beta,xi = np.linalg.solve(a_data,b_data)
+        fx4 = self.calc_fx4(alpha,beta,xi,gamma,self.ux_pow[1],self.ux_pow[2],
+                            self.ux_pow[4],self.ux_pow[6],umax)
+        #print(alpha,beta,gamma)
         return fx4
 
     ####for correlation######################
@@ -150,18 +153,38 @@ class Fxc(ModelXC):
         return (A+B*self.ux_pow[1]+C*self.ux_pow[2]+D*self.ux_pow[4])*np.exp(-E*self.ux_pow[2])
     
 
+    def calc_rho_x(self,gridID):
+        self.rho_x_stat_up = self.fx_up*self.rhoRUA[gridID]+self.f_b03_up*self.fx_down*self.rhoRUB[gridID]
+        self.rho_x_stat_down = self.fx_down*self.rhoRUB[gridID]+self.f_b03_down*self.fx_up*self.rhoRUA[gridID]
+        rho_x_stat = 1./2.*(1.+self.zeta[gridID])*self.rho_x_stat_up+\
+                    1./2.*(1.-self.zeta[gridID])*self.rho_x_stat_down
+        norm_rho_x_stat = lambda gamma2: 4.*np.pi*np.sum(self.uwei*self.ux_pow[2]*
+                                                    rho_x_stat*np.exp(-gamma2*self.ux_pow[2]))+1.
+        norm_0 = norm_rho_x_stat(0)
+        if np.abs(norm_0)<1e-10:
+            self.rho_x = rho_x_stat
+        else:
+            gamma2 = scipy.optimize.brentq(norm_rho_x_stat,-1e-3,1000)
+            self.rho_x = rho_x_stat*np.exp(-gamma2*self.ux_pow[2])
+        
+    
     def calc_exc_fxc(self,gridID):
         """
         To calculate exc for the model for a grid point
         It starts by calculating A and B of the correlation factor,
          calculates the parameters of fx, normalize the xc hole then calculate exc.
         """
-        #for correlation factor
-        A = self.calc_A(self.rs[gridID],self.zeta[gridID])
-        B = self.calc_B(self.rs[gridID],self.zeta[gridID])*self.kf[gridID] # because we have a function of u and not y
+
         #for exact exchange
-        norm_up=-1.
-        norm_down=-1.
+        if self.br_n_up[gridID]>1.:
+            self.br_n_up[gridID]=1.
+        if self.br_n_down[gridID]>1.:
+            self.br_n_down[gridID]=1.
+        norm_up=-1.#-self.br_n_up[gridID]
+        norm_down=-1.#-self.br_n_down[gridID]
+        self.f_b03_up = (1.-self.br_n_up[gridID])*2.
+        self.f_b03_down = (1.-self.br_n_down[gridID])*2.
+
         self.fx_up=self.calc_fx(norm_up,self.eps_x_exact_up[gridID],self.Q_up[gridID],self.rho_up[gridID],
                                                                 self.lap_up[gridID],self.rhoRUA[gridID])
 
@@ -174,19 +197,27 @@ class Fxc(ModelXC):
         else:
             self.fx_down=self.fx_up
 
-        self.rho_x = 1./2.*(1.+self.zeta[gridID])*self.fx_up*self.rhoRUA[gridID]+\
-                    1./2.*(1.-self.zeta[gridID])*self.fx_down*self.rhoRUB[gridID]
+
+        self.calc_rho_x(gridID)
+        #effective zeta
+        ontop_rho_x_stat = 0.5*(1.+self.zeta[gridID])*(self.rho_up[gridID]+self.f_b03_up*self.rho_down[gridID])+\
+                      0.5*(1.-self.zeta[gridID])*(self.rho_down[gridID]+self.f_b03_down*self.rho_up[gridID])
+        effective_zeta = np.sqrt(2.*ontop_rho_x_stat/self.rho_tot[gridID]-1)
+        #for correlation factor
+        #A = self.calc_A(self.rs[gridID],self.zeta[gridID])
+        #B = self.calc_B(self.rs[gridID],self.zeta[gridID])*self.kf[gridID] # because we have a function of u and not y
+        A = self.calc_A(self.rs[gridID],effective_zeta)
+        B = self.calc_B(self.rs[gridID],effective_zeta)*self.kf[gridID]
         #renormalize
         m1 = np.einsum("i,i,i->",self.uwei,self.ux_pow[1],self.rho_x)
         m2 = np.einsum("i,i,i->",self.uwei,self.ux_pow[2],self.rho_x)
         m3 = np.einsum("i,i,i->",self.uwei,self.ux_pow[3],self.rho_x)
         m4 = np.einsum("i,i,i->",self.uwei,self.ux_pow[4],self.rho_x)
         C = (-1/(4.*np.pi)-A*m2-B*m3)/m4
-        self.eps_xc_calc = 2.*np.pi*(m1*A+B*m2+C*m3)
-        #print(eps_xc*self.rho_tot[gridID],self.eps_x_exact_up[gridID]*self.rho_tot[gridID])       
+        eps_xc_calc = 2.*np.pi*(m1*A+B*m2+C*m3)
 
-        #eps_xc = 2.*np.pi*m1
-        return  self.eps_xc_calc*self.rho_tot[gridID]
+        #eps_xc_calc = 2.*np.pi*m1
+        return  eps_xc_calc*self.rho_tot[gridID]
 
 
     def calc_Etot_fxc(self):
